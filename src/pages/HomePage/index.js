6a675d2e708eb4356075f4e3b96d0d9f2504e2f6
Aramide <aramideotenaike@gmail.com>
2025-01-06 13:23:25 +0100

    added layout

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+function HomePage() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                Welcome to Swift Rates,<br/> a solution for your currency conversion problem!
+            </header>
+        </div>
+    );
+}
+
+export default HomePage;
